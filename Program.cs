2766e3d7be596fae2e270ca16600e88604c40ce5
Chris H <chrishallettprogrammer@gmail.com>
2025-08-31 10:43:12 +1000

    Added file input

--- Program.cs
+++ Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter file path to C# class input");
+
+        var inputPath = Console.ReadLine();
+
+        if (!File.Exists(inputPath))
+        {
+            throw new ApplicationException("File does not exist at given path");
+        }
+
+        //Read inputs
+        using StreamReader reader = new(inputPath);
+        string readText = reader.ReadToEnd();        
+
+        string[] lines = readText.Split(
+            new[] { "\r\n", "\r", "\n" },
+            StringSplitOptions.None
+        );
+
+        var firstSplit = lines[0];
+
     }
 }
